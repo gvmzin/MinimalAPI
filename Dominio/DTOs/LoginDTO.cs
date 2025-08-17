26998aa885cba52f919c831e6a074166acaba583
gvmzin <gustavomoraisgvm@gmail.com>
2025-08-17 07:57:40 -0300

    Iniciando Projeto

---
+++ Dominio/DTOs/LoginDTO.cs
@@ -0,0 +1,10 @@
+
+
+namespace minimalapi.DTOs{
+public class LoginDTO
+{
+    public string Email { get; set; } = default!;
+    public string Senha { get; set; } = default!;
+}
+
+}
---
+++ Program.cs
@@ -0,0 +1,21 @@
+
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+app.MapGet("/", () => "Olá pessoal");
+
+app.MapPost("/login", (minimalapi.DTOs.LoginDTO loginDTO) =>
+{
+    if (loginDTO.Email == "adm@teste.com" && loginDTO.Senha == "123456")
+    {
+        return Results.Ok("Login com sucesso");
+    }
+    else
+    {
+        return Results.Unauthorized();
+    }
+});
+
+
+app.Run();
+
